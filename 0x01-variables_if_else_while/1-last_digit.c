9c8c4e14d8dd303995f18a51b0cd546a12637d17
Prudence59 <pokeynwankwo@gmail.com>
2022-07-02 13:10:59 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
+/*
  * main - Determine if a random number is positive, negative or zero.
  *
  * Return: 0 on success
@@ -15,10 +15,12 @@ if (n < 0)
 {
         printf("%d is %s\n", n, "negative");
 }
+
 else if (n > 0)
 {
         printf("%d is %s\n", n, "positive");
 }
+
 else
 {
         printf("%d is %s\n", n, "zero");
