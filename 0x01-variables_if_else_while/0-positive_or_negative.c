45c542ceffd39aa9c60b5aec5a989f647b9933f2
Prudence59 <pokeynwankwo@gmail.com>
2022-07-02 09:43:58 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Determine if the last digit of a number is zero or not.
+ *
+ *   *
+ *
+ *    * Return: 0 on success
+ *
+ *     */
+
+int main(void)
+
+{
+
+	int n;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+
+
+	if (n <=0)
+
+	{
+
+		        printf("%d is %s\n", n, "and is 0");
+
+	}
+
+
+
+	else if (n > 5)
+
+	{
+
+		        printf("%d is %s\n", n, "and is greater than n");
+
+	}
+
+
+
+	else
+
+	{
+
+		        printf("%d is %s\n", n, "and is less than 6 and not 0");
+
+	}
+
+	return (0);
+
+}
+
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+My first Variacles if else while readme
