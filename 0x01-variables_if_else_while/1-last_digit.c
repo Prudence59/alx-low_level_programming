91e9bc1e03567aca20a4749da26e4eb70c2f8a9e
Prudence59 <pokeynwankwo@gmail.com>
2022-07-02 14:07:49 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,25 @@
-#include <stdlib.h>
+{#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - Determine if the last digit of a number is zero or not.
- *
- * Return: 0 on success
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-if (n <=0)
-{
-        printf("%d is %s\n", n, "and is 0");
-}
-else if (n > 5)
-{
-        printf("%d is %s\n", n, "and is greater than n");
-}
-else
-{
-        printf("%d is %s\n", n, "and is less than 6 and not 0");
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+    printf("%d is positive\n", n);
+if (n == 0)
+    printf("%d is zero\n", n);
+if (n < 0)
+    printf("%d is negative\n", n);
 }
 return (0);
 }
-
 
