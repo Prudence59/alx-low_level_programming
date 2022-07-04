a0c49e442dba8ca0e01e3c945b0dfec007516ee8
Prudence59 <pokeynwankwo@gmail.com>
2022-07-04 03:56:55 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *   * main - Prints base 10  numbers between  0 and 10
+ *   *
+ *   * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+char n;
+
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
+
