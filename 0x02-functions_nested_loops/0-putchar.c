be25d71c8bd627d77f44d7b0d1d1e28b80276b46
Prudence59 <pokeynwankwo@gmail.com>
2022-07-05 05:17:36 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main -Prnts exactly
- *
- * Reurns: Always 0 (success)
- */
-
+*main - Print _putchar
+*
+*Return: Always 0(Success)
+*/
 int main(void)
-
 {
-	printf("_putchar");
-		printf("\n");
-		return (0);
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
+	return (0);
 }
