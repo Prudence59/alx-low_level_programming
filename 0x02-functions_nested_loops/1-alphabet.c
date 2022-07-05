a3fac4f3e680d3aabf80abf81963ee5806cb9c80
Prudence59 <pokeynwankwo@gmail.com>
2022-07-05 14:37:23 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,11 @@
  * main - program that prints the alphabet in lowercase, followed by a new line.
 *Return: Always 0(success)
 */
-int main void
-char c;
-for(c=a;c<=z;c++);
+void print_alphabet(void)
 {
+	char c;
+for(c=a;c'<='z;c++)
+
 	_putchar(c);
 
 	_putchar('\n');
