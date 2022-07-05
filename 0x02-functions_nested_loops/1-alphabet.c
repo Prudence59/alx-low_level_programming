b5b81553e9d19a2033cd22a9bd512fa42cbd6693
Prudence59 <pokeynwankwo@gmail.com>
2022-07-05 05:30:12 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+ /*
+ * File: 1-alphabet.c
+ *
+ * Auth: kosi
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 97; alpha <= 122; alpha++)
+		_putchar(alpha);
+
+
+	putchar('\n');
+}
