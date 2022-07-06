3b0bfbd16ff53edcb7904d04ba7f75fcc62349b4
Prudence59 <pokeynwankwo@gmail.com>
2022-07-06 11:38:04 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  *  *print_alphabet - Alphabet in lowercase
  *   *
- *    *Return:0
+ *  *Return:0
  *    *
  */
 void print_alphabet(void)
@@ -11,10 +11,9 @@ void print_alphabet(void)
 	int r;
 
 for (r = 'a'; r <= 'z'; r++)
-{
+
 _putchar(r);
-(r++);
-}
+
 _putchar('\n');
-return (0);
+
 }
