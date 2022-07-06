7b0f0495be3c98714d4187d97646e846b39c3aae
Prudence59 <pokeynwankwo@gmail.com>
2022-07-06 04:19:37 -0700

    Task 14

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,28 +6,13 @@
  */
 int main(void)
 {
-int n, m, l;
+	int a, j;
 
-for (n = 48; n < 58; n++)
+	for (a = 1; a < 1024; a++)
 	{
-		for (m = 49; m < 58; m++)
-		{
-			for (l = 50; l < 58; l++)
-			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
+		if ((a % 3) == 0 || (a % 5) == 0)
+			j += a;
 	}
-	putchar('\n');
+	printf("%d\n", j);
 	return (0);
-}}
+}
