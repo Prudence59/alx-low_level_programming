e911876da94eb8771eff1874e7beec31dcff59f7
Prudence59 <pokeynwankwo@gmail.com>
2022-08-01 10:38:46 -0700

    New type documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,19 +4,26 @@
 /**
  * struct dog - header file that describes a new type struct dog
  * @name = the name of dog
+ *
  * @age = the age of dof
+ *
+ * @owner = the owner of the dog
  *
  * Description : the dog information continues
  */
+
 struct dog
 {
 	char *name;
 	float age;
-/**
- * @owner:the owner of the dog
- *
- * struct dog - the header file that decsribes a new type
- */
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
+typdef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*dog_H*/
