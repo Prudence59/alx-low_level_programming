f2472a1005ae8de25fbaa0d9f727634a57a4abc6
Prudence59 <pokeynwankwo@gmail.com>
2022-08-04 07:09:44 -0700

    Task 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name.c - Function that prints a name
+ * @name: pointer to a name
+ * @f: pointer to function
+ *
+ * Return : name
+ */
+
+void print_name(char *name, void (*f)(char *))
+
+	{
+	if (name != null && f != null)
+	return (name);
+	}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+Function Pointers in C Program
+Pointers of a function stores the address of the variable
+
