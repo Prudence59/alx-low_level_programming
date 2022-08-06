4d4d694466383e08eace7150e4eed9d93336fb10
Prudence59 <pokeynwankwo@gmail.com>
2022-08-06 05:22:28 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,9 +6,10 @@
 *@n : number of integers passed to the function
 *@separator : string to be printed between numbers
 *
-* Return: nothing
+* Return: void
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
+
 {
 	va_list valist;
 	unsigned int i;
